d06e07b7bb9283a6be49f724e223f657e7946ba9
Santiago Faci <santiago.faci@gmail.com>
2015-11-21 22:47:22 +0100

    Se añade la explicación de initializr de Spring

--- src/main/java/com/sfaci/eventoserver/Application.java
+++ src/main/java/com/sfaci/eventoserver/Application.java
@@ -14,6 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *      - ./gradlew bootRun
  *      - También se puede ejecutar el jar (con java -jar) que se genera en la carpeta 'build/libs' según el fichero 'build.gradle'
  *
+ *  El proyecto parte de un proyecto base creado con la herramienta Spring Initializr,
+ *  disponible en https://start.spring.io/. Conviene seleccionar ya de inicio las dependencias de Web, JPA y MySQL
+ *  De todas formas se pueden añadir luego a gradle y sincronizar el proyecto como se indica más arriba
+ *
  * @author Santiago Faci
  * @version curso 2015-2016
  */
